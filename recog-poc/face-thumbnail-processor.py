2821cbad656f27ec664c506276d10c992a5bed42
Assaf Yacobi <assafushy@gmail.com>
2022-02-17 21:25:14 +0200

    script and debug works with upload

--- recog-poc/face-thumbnail-processor.py
+++ recog-poc/face-thumbnail-processor.py
@@ -14,7 +14,7 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv[1:],"e:o:m:h:",["amqp-event=","output-dir=","minio-url=","--help="])
    except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
+      print('face-thumbnail-processor.py -e <amqp-file-event> -o <outputfile> -m<minio-url>')
       sys.exit(2)
    for opt, arg in opts:
       if opt in ("-h","--help"):
@@ -32,16 +32,17 @@ def main(argv):
    logging.info ('Minio url is  {} '.format(minioUrl))
 
    #parsing image url for download
-   #jsonEvent= json.loads(amqpEvent)
-   jsonEvent = {"EventName":"s3:ObjectCreated:Put","Key":"new-files/target.jpg","Records":[{"eventVersion":"2.0","eventSource":"minio:s3","awsRegion":"","eventTime":"2022-02-13T19:30:42.500Z","eventName":"s3:ObjectCreated:Put","userIdentity":{"principalId":"admin"},"requestParameters":{"principalId":"admin","region":"","sourceIPAddress":"10.42.2.39"},"responseElements":{"content-length":"0","x-amz-request-id":"16D36F47A44E4AA4","x-minio-deployment-id":"280668f5-b954-4764-b8ea-242228891bdd","x-minio-origin-endpoint":"http://10.42.2.39:9000"},"s3":{"s3SchemaVersion":"1.0","configurationId":"Config","bucket":{"name":"new-files","ownerIdentity":{"principalId":"admin"},"arn":"arn:aws:s3:::new-files"},"object":{"key":"target.jpg","size":640757,"eTag":"ce723304aa2c464398251be57346d480","contentType":"image/jpeg","userMetadata":{"content-type":"image/jpeg"},"sequencer":"16D36F47A9A8E798"}},"source":{"host":"10.42.2.39","port":"","userAgent":"MinIO (linux; amd64) minio-go/v7.0.21"}}]}
+   jsonEvent= json.loads(amqpEvent)
    pathArray = str(jsonEvent["Key"]).split("/")
    bucketName = pathArray[0]
    objectName=pathArray[1]
+   thumbnailsDir = "./thumbnails"
    #downloading image file
    filePath = downloadImageFile(bucketName,objectName,minioUrl,outputDir)
    #extracting face thumbnails
-   extractThumbnails(filePath,"./thumbnails")
+   extractThumbnails(filePath,thumbnailsDir)
    #uploading thumbnails
+   uploadThumbnails(thumbnailsDir,"thumbnails",minioUrl)
    
 def downloadImageFile(imageBucket,imageFile,minioUrl,destPath="."):
    logging.info('Downloading image {}'.format(imageBucket))   
@@ -64,15 +65,19 @@ def extractThumbnails(srcPhoto,thumbnailDestFolder):
       cropped_img = img.crop(area)
       cropped_img.save(thumbnailDestFolder + "/" +str(uuid.uuid4())+".jpg")
 
-def uploadThumbnails(destBucket,minioUrl):
+def uploadThumbnails(thumbnailsDir,destBucket,minioUrl):
    logging.info("uploading thumbnails")
    client = Minio(minioUrl,None,None,None,False)   # Create client with anonymous access.
-  #get files list
+   #get files list
+   fileList = os.listdir(thumbnailsDir)
    #for each file upload
-   try:
-      client.fput_object(destBucket)
-   except:
-      logging.error("erron downloading photo : {} to bucket {}".format(["image",destBucket]))
+   for file in fileList:
+      try:
+         filePath = thumbnailsDir+"/"+file
+         client.fput_object(bucket_name=destBucket,file_path=filePath,object_name=file)
+      except Exception as e:
+         logging.error(e)
+         logging.error("erron downloading photo : {} to bucket {}".format(["image",destBucket]))
 
 if __name__ == '__main__':
    main(sys.argv)
